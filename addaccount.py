6a3d96836638ca9c08c25a0ec6f43e51f5846067
iarc <hexiaoyu42@outlook.com>
2016-11-13 21:22:09 +0800

    add

---
+++ addaccount.py
@@ -0,0 +1,9 @@
+mport keystoneclient.v2_0.client as ksclient
+# Replace the method arguments with the ones from your local config
+keystone = ksclient.Client(auth_url="http://192.168.1.136:35357/v2.0",
+                           username="admin",
+                           password="admin",
+                           tenant_name="demo")
+glance_service = keystone.services.create(name="glance",
+                            service_type="image",
+                            description="OpenStack Image Service")
