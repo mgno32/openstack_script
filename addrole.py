7a8680600f845fc424bfc3a379f0177b6cd76886
iarc <hexiaoyu42@outlook.com>
2016-11-14 21:43:19 +0800

    add a role

---
+++ addrole.py
@@ -0,0 +1,14 @@
+import keystoneclient.v2_0.client as ksclient
+endpoint = "http://192.168.1.136:35357/v2.0"
+admin_token = "devstack"
+
+keystone = ksclient.Client(endpoint=endpoint, token=admin_token)
+user_role = keystone.roles.create("user")
+admin_role = keystone.roles.create("admin")
+xiaoyu_tenant = keystone.tenants.create(tenant_name="roleMgno32",
+                        description="Xiaoyu as a role.",
+                        enabled=True)
+admin_user = keystone.users.create(name="admin1",
+                password="ADMIN",
+                email="user2@example.com", tenant_id=xiaoyu_tenant.id)
+keystone.roles.add_user_role(admin_user, admin_role, xiaoyu_tenant)
